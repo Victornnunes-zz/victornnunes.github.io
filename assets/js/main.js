a5f3b6ce3e1ca628f8cdf4cee2de225df1a6dd36
Victor Nunes <victornnunes@Outlook.com.br>
2019-09-11 11:45:17 -0300

    correções

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,13 @@
 jQuery(document).ready(function($) {
     $(".scroll").click(function(event){
-    event.preventDefault();
-    $('html,body').animate({scrollTop:$(this.hash).offset().top}, 600);
+        event.preventDefault();
+        $('html,body').animate({scrollTop:$(this.hash).offset().top}, 600);
     });
+});
+
+$(document).ready(function() {
+    $('#menu-lateral').click(function () { return false; });
+    $().click(function () { 
+        $('#menu-lateral').hide()
     });
+});
